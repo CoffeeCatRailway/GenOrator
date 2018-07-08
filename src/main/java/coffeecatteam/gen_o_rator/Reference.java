534c0986549861c7291ed18eac590c5ca7abb0a0
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-07-08 10:18:49 +1000

    Changed version to show beta

--- src/main/java/coffeecatteam/gen_o_rator/Reference.java
+++ src/main/java/coffeecatteam/gen_o_rator/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MODID = "gen_o_rator";
     public static final String NAME = "GenOrator";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.1-beta";
 
     public static final String CLIENTPROXY = "coffeecatteam.gen_o_rator.proxy.ProxyClient";
     public static final String COMMONPROXY = "coffeecatteam.gen_o_rator.proxy.ProxyCommon";
