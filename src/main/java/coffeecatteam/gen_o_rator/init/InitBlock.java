08aedcb370286435a78a46b51fe7bf14b186e256
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-07-14 16:56:08 +1000

    Updated InitItem/Block

--- src/main/java/coffeecatteam/gen_o_rator/init/InitBlock.java
+++ src/main/java/coffeecatteam/gen_o_rator/init/InitBlock.java
@@ -1,11 +1,15 @@
 package coffeecatteam.gen_o_rator.init;
 
+import coffeecatteam.gen_o_rator.GenOrator;
 import coffeecatteam.gen_o_rator.Reference;
 import coffeecatteam.gen_o_rator.objects.blocks.BlockCable;
 import coffeecatteam.gen_o_rator.objects.blocks.BlockCoalGenerator;
+import coffeecatteam.gen_o_rator.objects.blocks.BlockDriedUpLog;
+import coffeecatteam.gen_o_rator.objects.blocks.base.BlockBase;
 import coffeecatteam.gen_o_rator.objects.tileentity.TileCable;
 import coffeecatteam.gen_o_rator.objects.tileentity.TileCoalGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -14,15 +18,27 @@ public class InitBlock {
     /* Generators */
     public static final Block COAL_GENERATOR = new BlockCoalGenerator("coal_generator");
 
-    // Cables
+    /* Cables */
     public static final Block CABLE_IRON = new BlockCable("iron", "blockCable", 500, 50, 50);
     public static final Block CABLE_SILVER = new BlockCable("silver", "blockCable", 1000, 50, 100);
     public static final Block CABLE_ALUMINIUM = new BlockCable("aluminium", "blockCable", 1500, 100, 150);
     public static final Block CABLE_COPPER = new BlockCable("copper", "blockCable", 2000, 200, 200);
 
+    /* Ores */
+    public static final Block ORE_SILVER = new BlockBase("silver_ore", 2.0F, 4.0F, Material.ROCK, false, 1, GenOrator.TAB);
+    public static final Block ORE_ALUMINIUM = new BlockBase("aluminium_ore", 2.0F, 4.0F, Material.ROCK, false, 1, GenOrator.TAB);
+    public static final Block ORE_COPPER = new BlockBase("copper_ore", 2.0F, 4.0F, Material.ROCK, false, 1, GenOrator.TAB);
+
+    /* Other */
+    public static final Block DRIED_UP_LOG = new BlockDriedUpLog("dried_up_log", GenOrator.TAB);
+
 	public static void init() {
         register(COAL_GENERATOR);
         register(CABLE_IRON, CABLE_SILVER, CABLE_ALUMINIUM, CABLE_COPPER);
+
+        register(ORE_SILVER, ORE_ALUMINIUM, ORE_COPPER);
+
+        register(DRIED_UP_LOG);
 	}
 
 	public static void registerTileEntities() {
--- src/main/java/coffeecatteam/gen_o_rator/init/InitItem.java
+++ src/main/java/coffeecatteam/gen_o_rator/init/InitItem.java
@@ -1,14 +1,55 @@
 package coffeecatteam.gen_o_rator.init;
 
+import coffeecatteam.gen_o_rator.GenOrator;
+import coffeecatteam.gen_o_rator.objects.items.ItemBase;
+import coffeecatteam.gen_o_rator.objects.items.ItemCraftingComponent;
+import coffeecatteam.gen_o_rator.objects.items.ItemSapCollector;
 import net.minecraft.item.Item;
 
 public class InitItem {
 
-    
+    /* Ingot */
+    public static final Item INGOT_SILVER = new ItemBase("ingot_silver", "ingotSilver", GenOrator.TAB);
+    public static final Item INGOT_ALUMINIUM = new ItemBase("ingot_aluminium", "ingotAluminium", GenOrator.TAB);
+    public static final Item INGOT_COPPER = new ItemBase("ingot_copper", "ingotCopper", GenOrator.TAB);
 
-	public static void init() {
+    /* Wire */
+    public static final Item WIRE_IRON = new ItemBase("wire_iron", "wireIron", GenOrator.TAB);
+    public static final Item WIRE_SILVER = new ItemBase("wire_silver", "wireSilver", GenOrator.TAB);
+    public static final Item WIRE_ALUMINIUM = new ItemBase("wire_aluminium", "wireAluminium", GenOrator.TAB);
+    public static final Item WIRE_COPPER = new ItemBase("wire_copper", "wireCopper", GenOrator.TAB);
 
-	}
+    /* Crafting Components */
+    public static final Item RF_CONVERTER = new ItemCraftingComponent("rf_converter");
+    public static final Item MOTOR = new ItemCraftingComponent("motor");
+    public static final Item ENERGY_CONNECTOR = new ItemCraftingComponent("energy_connector");
+
+    public static final Item BLANK_CIRCUIT = new ItemCraftingComponent("blank_circuit");
+    public static final Item BASIC_CIRCUIT = new ItemCraftingComponent("basic_circuit");
+    public static final Item ADVANCED_CIRCUIT = new ItemCraftingComponent("advanced_circuit");
+
+    /* Other */
+    public static final Item TREE_SAP = new ItemBase("tree_sap", "", GenOrator.TAB);
+    public static final Item RUBBER = new ItemBase("rubber", "itemRubber", GenOrator.TAB);
+    public static final Item PLASTIC = new ItemBase("plastic", "itemPlastic", GenOrator.TAB);
+
+    public static final Item SAP_COLLECTOR = new ItemSapCollector("sap_collector");
+
+    public static void init() {
+        /* Ingot */
+        register(INGOT_SILVER, INGOT_ALUMINIUM, INGOT_COPPER);
+
+        /* Wire */
+        register(WIRE_IRON, WIRE_SILVER, WIRE_ALUMINIUM, WIRE_COPPER);
+
+        /* Crafting Components */
+        register(RF_CONVERTER, MOTOR, ENERGY_CONNECTOR);
+        register(TREE_SAP, RUBBER, PLASTIC);
+        register(BLANK_CIRCUIT, BASIC_CIRCUIT, ADVANCED_CIRCUIT);
+
+        /* Other */
+        register(SAP_COLLECTOR);
+    }
 
     private static void register(Item... items) {
         for (Item item : items)
